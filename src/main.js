26d9ce542848697e1ce1b44bacbde68fcd4392a2
hudgieri <hudgieri@hotmail.com>
2020-07-23 11:14:49 -0300

    meu segundo commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm'
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
@@ -10,3 +17,6 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+
+//main.js local de importação global e definição de funções globais
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,42 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import loco from '../views/loco'
+import Aprendendo from "../views/Aprendendo";
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/test2',
+        name: 'Exercicios',
+        component: Aprendendo
+    },
+    {
+        path: '/test',
+        name: 'sofoda',
+        component: loco
+        // Componente que será interpretado pela rota
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
